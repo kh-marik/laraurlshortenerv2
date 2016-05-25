cefc28a0541a4faf98ee7900c9e025836239cfb0
Marik <zombie_squad@mail.ru>
2016-05-26 02:34:03 +0500

    Change register mech - user 'admin' is admin by default after register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,10 +63,12 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
+        $is_admin = $data['name'] == 'admin' ? 1 : 0;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'is_admin' => $is_admin
         ]);
     }
 }
