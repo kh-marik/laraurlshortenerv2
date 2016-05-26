5dcccb84fedc468165b50bdc235b373ad59ec3c7
Marik <zombie_squad@mail.ru>
2016-05-27 04:31:47 +0500

    Add app configuration file to config folder

---
+++ config/urlshortener.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'name'               => "Url Shortener",
+        'title'              => 'Url Shortener',
+        'description'        => 'Simple Url Shortener Script on Laravel 5.2',
+        'author'             => 'Marik',
+        'links_per_page'     => 25
+    ];
